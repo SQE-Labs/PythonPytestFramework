e48b34de26523b410a40eb4223410207fc7c37b0
parveen9719 <parveen.sharma@sqelabs.com>
2024-03-01 16:14:02 +0530

    updated

--- Tests/Config/config.py
+++
@@ -1,7 +0,0 @@
-class TestData:
-    BASE_URL = "https://app-stage.comcate.com/agencies/212/login"
-    USERNAME="MarkShane1923@yopmail.com"
-    PASSWORD="Hello@1234"
-    CASELIST_PAGETITLE="Cases"
-    CREAT_SUBMISSION_URL=""
-    SEARCH_LOCATION="TEXAS CITY"
